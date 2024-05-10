6eb7cd4a238a67240a2cda6b7835ee11453d92e2
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-05-10 14:28:04 +0200

    Initial commit

---
+++ src/main/java/develhope/esercizio91servizilogging/Esercizio91ServiziLoggingApplication.java
@@ -0,0 +1,12 @@
+package develhope.esercizio91servizilogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio91ServiziLoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Esercizio91ServiziLoggingApplication.class, args);
+    }
+}
---
+++ src/test/java/develhope/esercizio91servizilogging/Esercizio91ServiziLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package develhope.esercizio91servizilogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio91ServiziLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
